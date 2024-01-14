f8980d8a7ec01afa3f832cc5379d24dc086c370b
resilucia024 <resilucia024@gmail.com>
2024-01-14 10:10:22 -0800

    nav edit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import GetStarted from '../components/GetStarted';
 import Header from '../components/Header';
 import Navbar from './../components/Navbar';
 import Footer from './../components/Footer/Footer';
+import { useNavigate } from 'react-router-dom';
 const Home = () => {
 
   const navigate = useNavigate();
